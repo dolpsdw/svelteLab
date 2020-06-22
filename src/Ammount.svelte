a995b81473c1c9479a46b045033512512e903e58
dolpsdw <dolpsdw@gmail.com>
2020-06-22 23:53:29 +0200

    0000 and erase

--- src/Ammount.svelte
+++ src/Ammount.svelte
@@ -19,27 +19,32 @@
 
   const handleWinInput = (e) => {
     console.log("handledWinStart", e);
+    if (e.inputType === "deleteContentBackward" && lastValidWin < 10) {
+      // single digit will put a 0
+      e.target.value = 0;
+    }
     if (isNaN(e.target.valueAsNumber)) {
       // [+, -, e] trigger this
       console.log(lastValidWin);
       currentWin = lastValidWin + 1 - 1;
       // currentWin reAsignation does not work on this update cycle
-      // Javascript to the rescue
-      e.target.value = lastValidWin;
-      // And this is not firing handleWinInput again
     } else {
       lastValidWin = e.target.valueAsNumber;
     }
+
+    // Javascript to the rescue also reformat 00000000 case
+    e.target.value = lastValidWin;
+    // And this is not firing handleWinInput again
   };
   const handleRiskInput = (e) => {
     console.log("handledRiskStart", e);
     if (isNaN(e.target.valueAsNumber)) {
       console.log(lastValidRisk);
       currentRisk = lastValidRisk;
-      e.target.value = lastValidRisk;
     } else {
       lastValidRisk = e.target.valueAsNumber;
     }
+    e.target.value = lastValidRisk;
   };
 </script>
 
