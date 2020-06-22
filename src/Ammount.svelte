afbc3439251f5a291c5e01397932b26c96fca461
dolpsdw <dolpsdw@gmail.com>
2020-06-22 23:15:17 +0200

    SvelteBind

--- src/Ammount.svelte
+++ src/Ammount.svelte
@@ -1,11 +1,55 @@
 <script>
   /** configurable separator , and .
    *   +- characters not allowed
+   * edgeCases:
+   * inputType: "insertFromPaste"
+   * 000000000000000024
+   * 3,,,,,
+   * 3e3e3e3e3e
    */
   export let riskAble = true;
   export let winAble = true;
-  const decimalChar = ",";
+  export let decimalChar = ",";
+
+  let lastValidWin = 0;
+  let lastValidRisk = 0;
+
+  let currentWin = 0;
+  let currentRisk = 0;
+
+  const handleWinInput = (e) => {
+    console.log("handledWinStart", e);
+    if (isNaN(e.target.valueAsNumber)) {
+      // [+, -, e] trigger this
+      console.log(lastValidWin);
+      currentWin = lastValidWin + 1 - 1;
+      // currentWin reAsignation does not work on this update cycle
+    } else {
+      lastValidWin = e.target.valueAsNumber;
+    }
+  };
+  const handleRiskInput = (e) => {
+    console.log("handledRiskStart", e);
+    if (isNaN(e.target.valueAsNumber)) {
+      console.log(lastValidRisk);
+      currentRisk = lastValidRisk;
+    } else {
+      lastValidRisk = e.target.valueAsNumber;
+    }
+  };
 </script>
 
-<input type="number" disabled={!riskAble} />
-<input type="number" disabled={!winAble} />
+<!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/number -->
+<!-- input event get fired each time text value changes -->
+<input
+  type="number"
+  disabled={!riskAble}
+  min="1"
+  on:input={handleWinInput}
+  bind:value={currentWin} />
+<input
+  type="number"
+  disabled={!winAble}
+  min="1"
+  on:input={handleRiskInput}
+  bind:value={currentRisk} />
